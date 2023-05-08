6c961a39772624ac9dbb4db19a43b2b9c21866e8
Charles <charlieu2000@gmail.com>
2023-05-08 21:33:23 +0100

    'message'

---
+++ CODE/users/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    profile_picture = models.ImageField(default="default2.jpg", upload_to="profile_pics")
+    bio = models.CharField(max_length=100)
+
+
+    def __str__(self):
+        return f"{self.user} profile"
+
+# @receiver(post_save, sender=User)
+# def create_user_profile(sender, instance, created, **kwargs):
+#     if created:
+#         Profile.objects.create(user=instance)
+
+# @receiver(post_save, sender=User)
+# def save_user_profile(sender, instance, **kwargs):
+#     instance.profile.save()
